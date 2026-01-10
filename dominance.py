b20c8c7d508724bdd36c401be4cd27975c1757b4
seran261 <seran261@gmail.com>
2026-01-10 23:02:12 +0530

    Update dominance.py

--- dominance.py
+++ dominance.py
@@ -4,33 +4,21 @@ from config import BTC_DOMINANCE_MAX, ALLOW_BTC_ALWAYS
 
 COINGECKO_GLOBAL = "https://api.coingecko.com/api/v3/global"
 
-_last_fetch = 0
-_cached_dominance = None
-
+_last = 0
+_cache = None
 
 def get_btc_dominance():
-    global _last_fetch, _cached_dominance
-
-    if time.time() - _last_fetch < 300 and _cached_dominance:
-        return _cached_dominance
-
+    global _last, _cache
+    if time.time() - _last < 300 and _cache:
+        return _cache
     data = requests.get(COINGECKO_GLOBAL, timeout=10).json()
-    dominance = data["data"]["market_cap_percentage"]["btc"]
-
-    _cached_dominance = dominance
-    _last_fetch = time.time()
-    return dominance
-
+    _cache = data["data"]["market_cap_percentage"]["btc"]
+    _last = time.time()
+    return _cache
 
 def btc_dominance_ok(symbol, side):
-    dominance = get_btc_dominance()
-
-    # Always allow BTC trades
     if symbol.startswith("BTC") and ALLOW_BTC_ALWAYS:
         return True
-
-    # Block ALT BUY in high BTC dominance
-    if side == "BUY" and dominance > BTC_DOMINANCE_MAX:
+    if side == "BUY" and get_btc_dominance() > BTC_DOMINANCE_MAX:
         return False
-
     return True
