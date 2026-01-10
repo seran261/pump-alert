9a3ab66d76d4cca7279e51164c80072fb4c47f9e
seran261 <seran261@gmail.com>
2026-01-10 21:32:23 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,11 @@
+# telegram.py
 import requests
 import os
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-def send_signal(market, symbol, tf, side, price):
+def send_signal(market, symbol, tf, side, entry, tp, sl):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
         print("❌ Telegram env vars missing")
         return
@@ -15,11 +16,12 @@ def send_signal(market, symbol, tf, side, price):
         f"Symbol: {symbol}\n"
         f"TF: {tf}\n"
         f"Side: {side}\n"
-        f"Price: {price}"
+        f"Entry: {entry}\n"
+        f"🎯 Take Profit: {tp}\n"
+        f"🛑 Stop Loss: {sl}"
     )
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-
     r = requests.post(url, json={
         "chat_id": TELEGRAM_CHAT_ID,
         "text": msg
