0636660f3f48ce497f8eafd20dac8bfaa7c879d9
seran261 <seran261@gmail.com>
2026-01-10 21:09:37 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,11 +1,20 @@
+# telegram.py
 import requests
-from config import TELEGRAM_TOKEN, CHAT_ID
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-def send(msg):
+def send_signal(market, symbol, tf, side, price):
+    msg = (
+        f"📡 LONG TERM SIGNAL\n"
+        f"Market: {market}\n"
+        f"Symbol: {symbol}\n"
+        f"TF: {tf}\n"
+        f"Side: {side}\n"
+        f"Price: {price}"
+    )
+
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
-        "chat_id": CHAT_ID,
-        "text": msg,
-        "parse_mode": "Markdown"
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": msg
     }
-    requests.post(url, json=payload, timeout=10)
+    requests.post(url, json=payload)
